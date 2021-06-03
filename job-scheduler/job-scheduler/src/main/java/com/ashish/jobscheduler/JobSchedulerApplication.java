65b48527e0cdde680ae5815487a4276bffbc49fc
Ashish Rana <ashish.rana44@gmail.com>
2021-06-03 22:54:23 +0530

    push dir to github-first

---
+++ job-scheduler/job-scheduler/src/main/java/com/ashish/jobscheduler/JobSchedulerApplication.java
@@ -0,0 +1,17 @@
+package com.ashish.jobscheduler;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableBatchProcessing
+@SpringBootApplication
+public class JobSchedulerApplication {
+
+	public static void main(String[] args)
+	{
+
+		SpringApplication.run(JobSchedulerApplication.class, args);
+	    System.out.println("India");
+	}
+
+}
